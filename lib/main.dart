f6d1182df66f55d2875204422d6652a8274c283a
Tan Aloysius <tanaloysius@tans-air.lan>
2021-08-18 19:00:33 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'screen0.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Screen0(),
+    );
+  }
+}
---
+++ lib/screen0.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'screen1.dart';
+import 'screen2.dart';
+
+class Screen0 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.purple,
+        title: Text('Screen 0'),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            RaisedButton(
+              color: Colors.red,
+              child: Text('Go To Screen 1'),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return Screen1();
+                    },
+                  ),
+                );
+              },
+            ),
+            RaisedButton(
+              color: Colors.blue,
+              child: Text('Go To Screen 2'),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context){
+                    return Screen2();
+                  }),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
